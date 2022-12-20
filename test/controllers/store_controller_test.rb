ed3ae31675d40e5bff94024f51fa60d4a89e3bcc
James Waters <jwaters1993@outlook.com>
2022-12-19 22:04:41 -0500

    Created new controlled to handle customer interactions Implemented the default index action Wrote functional tests for store controller Implemented fragment caching for portions of the page

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,5 +4,11 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    # Verify response contains layout, product infomation, and number formatting
+    assert_select 'nav.side_nav a', minumum: 4
+    assert_select 'main ul.catalog li', 3
+    assert_select 'h2', 'Programming Ruby 1.9'
+    assert_select '.price', /\$[,\d]+\.\d/
+  
   end
 end
